a7f3b2b73abff8df780255f0f05a40f69752e5ad
Kol124 <65050225+Kol124@users.noreply.github.com>
2021-10-25 15:17:56 +0100

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+// import { Route, Link } from 'react-router-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {/* {this.renderHeader()}
+        <Route path="/post" component={CommentBox} />
+        <Route path="/" exact component={CommentList} /> */}
+      </div>
+    );
+  }
+}
+
+export default App;
